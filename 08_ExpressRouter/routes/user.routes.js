376a54ca6707a7c58fcee39c6bde10fd01b0d3b2
Tai Le <alexsandro134@gmail.com>
2018-09-24 23:29:44 +0700

    update MVC_Routes

---
+++ 08_ExpressRouter/routes/user.routes.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var shortid = require('shortid');
+
+var db = require('../db');
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.render('users/index.pug', {
+        users: db.get('users').value()
+    });
+});
+
+router.get('/search', function (req, res) {
+    var q = req.query.q;
+    var matchedUsers = db.get('users').value().filter(function (user) {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    })
+
+    res.render('users/index.pug', {
+        searchText: q,
+        users: matchedUsers
+    });
+});
+
+router.get('/create', function (req, res) {
+    res.render('users/create.pug');
+});
+
+router.get('/:id', function(req, res) {
+    var id = req.params.id;
+
+    var user = db.get('users').find({id: id}).value();
+
+    res.render('users/view.pug', {
+        user: user
+    });
+});
+
+router.post('/create', function (req, res) {
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
+});
+
+module.exports = router;
---
+++ 09_ControllerMVC/routes/user.routes.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var shortid = require('shortid');
+
+var db = require('../db');
+var controller = require('../controllers/user.controller');
+
+var router = express.Router();
+
+router.get('/', controller.index);
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/:id', controller.get);
+
+router.post('/create', controller.postCreate);
+
+module.exports = router;
