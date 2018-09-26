7f045031f79ca710e178c8cb060a08583037602d
Tai Le <alexsandro134@gmail.com>
2018-09-26 23:55:07 +0700

    Server validation

---
+++ 12_ServerSideValidation/controllers/user.controller.js
@@ -0,0 +1,61 @@
+var db = require('../db');
+var shortid = require('shortid');
+
+module.exports.index = function (req, res) {
+    res.render('users/index.pug', {
+        users: db.get('users').value()
+    });
+};
+
+module.exports.search = function (req, res) {
+    var q = req.query.q;
+    var matchedUsers = db.get('users').value().filter(function (user) {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    })
+
+    res.render('users/index.pug', {
+        searchText: q,
+        users: matchedUsers
+    });
+};
+
+module.exports.create = function (req, res) {
+    res.render('users/create.pug');
+};
+
+module.exports.get = function (req, res) {
+    var id = req.params.id;
+
+    var user = db.get('users').find({
+        id: id
+    }).value();
+
+    res.render('users/view.pug', {
+        user: user
+    });
+};
+
+module.exports.postCreate = function (req, res) {
+    req.body.id = shortid.generate();
+
+    var errors = [];
+
+    if (!req.body.name) {
+        errors.push('Name is required.');
+    }
+
+    if (!req.body.phone) {
+        errors.push('Phone is required.');
+    }
+
+    if (errors.length) {
+        res.render('users/create.pug', {
+            errors: errors,
+            values: req.body
+        });
+        return;
+    }
+
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
+};
