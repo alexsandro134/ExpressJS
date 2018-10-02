05be71003de6066e6cc64c547e3c39e3117b03db
Tai Le <alexsandro134@gmail.com>
2018-10-02 21:15:29 +0700

    update SignedCookie

---
+++ 17_SignedCookie/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+var userRoute = require('./routes/user.routes');
+var authRoute = require('./routes/auth.routes')
+
+var authMiddleware = require('./middlewares/auth.middlewares');
+
+var port = 5000;
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencodeds
+app.use(cookieParser('23p13sdslh193@13'));
+
+app.get('/', function (req, res) {
+    res.render('index.pug', {
+        name: 'AAA'
+    });
+});
+
+app.use('/users', authMiddleware.requireAuth, userRoute);
+app.use('/auth', authRoute);
+
+
+app.listen(port, function () {
+    console.log('Server is listening from port ' + port)
+});
