c489f4694486256a71f200dd22a5361d2b438335
Tai Le <alexsandro134@gmail.com>
2018-10-29 07:13:29 +0700

    update pagination

--- MyBookSite/controllers/books.controller.js
+++ MyBookSite/controllers/books.controller.js
@@ -2,12 +2,30 @@
 //     object
 // };
 
+// fomular for pagination
+// Items in page n, with x items per page
+// begin = (n - 1) * x
+// end = (n - 1) * x + x = n * x
+// items = array.slice(begin, end)
+
 var db = require('../db');
 var shortid = require('shortid');
 
 module.exports.index = function (req, res) {
+    var page = parseInt(req.query.page) || 1; // n
+    var perPage = 8; // x
+ 
+    var start = (page - 1) * perPage;
+    var end = page * perPage;
+
+    var numOfProducts = db.get('books').size().value();
     res.render('books/index', {
-        books: db.get('books').value()
+        //books: db.get('books').value().slice(start, end)
+
+        // using Lodash
+        books: db.get('books').drop(start).take(perPage).value(),
+        page: page,
+        total: numOfProducts
     });
 };
 
